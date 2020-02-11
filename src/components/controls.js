78fcc94a12845090b6f412bfb9de7eb181119b27
aesher9o1 <aashiskumar986@gmail.com>
2020-02-12 00:00:18 +0530

    stopwatch initiated

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,42 +1,51 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { withTheme } from 'styled-components';
 import styled from 'styled-components'
 
-function Controls (){
-    const [timeElapsed, setTimeElapsed] = useState(0)
-    const FloatingLayout = styled.div`
-        background : ${props=>props.theme.colorSeconday};
-        box-shadow : ${props=>props.theme.primaryBoxShadow};
-        display: block;
-        position: absolute;
-        width:380px!important;
-        margin:auto;
-        left:0;
-        right:0;
-        text-align:centre;
-        border-radius:${props=>props.theme.radius};
-        padding: ${props=>props.theme.padding};
-        margin-top: 2em
-    ` 
+const FloatingLayout = styled.div`
+background : ${props => props.theme.colorSeconday};
+box-shadow : ${props => props.theme.primaryBoxShadow};
+display: block;
+position: absolute;
+width:380px!important;
+margin:auto;
+left:0;
+right:0;
+text-align:centre;
+border-radius:${props => props.theme.radius};
+padding: ${props => props.theme.padding};
+margin-top: 2em
+`
+
+function Controls() {
+    const [seconds, setSeconds] = useState(0);
 
 
-    useEffect(()=>{
+    const padZeros = (i) => {
+        return (i < 10) ? `0 ${i}` : i
+    }
 
-    },[])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
 
 
     return (<FloatingLayout>
-           <ul>
-               <li>
-                    ⛔️
+        <ul>
+            <li>
+                ⛔️
                </li>
-               <li>
-                   Timer
+            <li>
+                {seconds}
+            </li>
+            <li>
+                ➕
                </li>
-               <li>
-               ➕
-               </li>
-           </ul>
+        </ul>
     </FloatingLayout>)
 }
 
