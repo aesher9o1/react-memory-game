234a86573ec8f5bd1dbac5607ecd9b78caf225f8
aesher9o1 <aashis@internshala.com>
2020-02-11 15:06:33 +0530

    Initial commit

---
+++ src/utils/repository.js
@@ -0,0 +1,43 @@
+export const  EmojiRepository = [
+    "🐷",
+    "🦉",
+    "🦍",
+    "🐏",
+    "🦌",
+    "🐿",
+    "🦔",
+    "🦃",
+    "🕊",
+    "🦛",
+    "🐪",
+    "🦀",
+    "🦐",
+    "🦀",
+    "🐡 ",
+    "🐢",
+    "🦖",
+    "🐙",
+    "🐌",
+    "🐮"
+]
+
+export const POSITIVE_REINFORCEMENTS = [
+    "You are smokn'g..🚬",
+    "Fireeeeeeeeeee🔥",
+    "Somebody has been flawless lateley💅🏻",
+    "Are you a hacker?👨🏽‍💻",
+    "Never seen someone so perfect 🙈"
+]
+
+export const NEGATIVE_REINFORCEMENTS = [
+    "Errr!! Are you even trying?👀",
+    "Quick fact, Almonds are good for memory🙈",
+    "Alert!! The system has been infected by noob9o1🐏",
+    "I bet if i give you the answer you would have forgot the question🤸‍♂️",
+    "I will find you and i will kill you 🦉",
+    "I have lost hope 🦛",
+    "You droped 🧠 in the backyard"
+]
+
+export const LIMIT_HIGH = "Woahh!!! Slow down cowboy🤙"
+export const LIMIT_LOW = "Are you sure you want to play?😑"
